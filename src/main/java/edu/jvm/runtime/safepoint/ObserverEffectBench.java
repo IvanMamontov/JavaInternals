aca1cc1ca79b9c863407cf5fc0838f2baf811ebf
Ivan Mamontov <ivan.mamontov@gmail.com>
2014-09-24 11:59:16 +0400

    Lies, damned lies, and profilers

--- src/main/java/edu/jvm/runtime/safepoint/ObserverEffectBench.java
+++ src/main/java/edu/jvm/runtime/safepoint/ObserverEffectBench.java
@@ -16,13 +16,38 @@ import java.util.concurrent.TimeUnit;
 @Fork(value = 3, jvmArgs = {"-XX:+PrintSafepointStatistics", "-XX:PrintSafepointStatisticsTimeout=30", "-XX:-UseBiasedLocking"})
 @Warmup(iterations = 1, time = 1, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 2, time = 20, timeUnit = TimeUnit.SECONDS)
-public class SafepointBench {
+public class ObserverEffectBench {
 
     int i = 0;
 
     @Benchmark
-    @Group("g")
+    @Group("observing")
     @GroupThreads(1)
+    public double observed_slowpoke() {
+        return slowpoke();
+    }
+
+    @Benchmark
+    @Group("free")
+    @GroupThreads(1)
+    public void free_flash() {
+        flash();
+    }
+
+    @Benchmark
+    @Group("free")
+    @GroupThreads(1)
+    public double free_slowpoke() {
+        return slowpoke();
+    }
+
+    @Benchmark
+    @Group("observing")
+    @GroupThreads(1)
+    public void observed_flash() {
+        flash();
+    }
+
     public double slowpoke() {
         double d = 0;
         //no chance to unroll
@@ -32,15 +57,12 @@ public class SafepointBench {
         return d;
     }
 
-    @Benchmark
-    @Group("g")
-    @GroupThreads(1)
     public void flash() {
         i++;
     }
 
     @Benchmark
-    @Group("g")
+    @Group("observing")
     @GroupThreads(1)
     public int sampler() throws Exception {
         Thread.sleep(20);
@@ -50,7 +72,7 @@ public class SafepointBench {
 
     public static void main(String[] args) throws RunnerException {
         Options options = new OptionsBuilder()
-                .include(SafepointBench.class.getName())
+                .include(ObserverEffectBench.class.getName())
                 .verbosity(VerboseMode.NORMAL)
                 .build();
         new Runner(options).run();
