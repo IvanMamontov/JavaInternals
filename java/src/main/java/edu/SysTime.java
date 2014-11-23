8fdb25e9b59ef49d3674f6de3ccb92714cb68984
Ivan Mamontov <ivan.mamontov@gmail.com>
2014-11-23 23:10:55 +0300

    JNI for linux platform was corrected

--- java/src/main/java/edu/SysTime.java
+++ java/src/main/java/edu/SysTime.java
@@ -1,21 +1,20 @@
 package edu;
 
-//
-// Definition of native methods to get various clock values from Java
-//
+/**
+ *
+ */
 public class SysTime
 {
 
     static {
         try {
-            System.load("/Users/imamontov/projects/JMH_examples/native/macosx/target/libutil.jnilib");
+            System.loadLibrary("util");
         } catch (UnsatisfiedLinkError e) {
             System.err.println("Native code library failed to load.\n" + e);
             System.exit(1);
         }
     }
 
-//    public native static long clocktime();
     public native static long cpuidrdtsc();
     public native static long rdtsc();
     public native static long rdtscp();
@@ -30,12 +29,10 @@ public class SysTime
             long tsc = SysTime.rdtsc();
             long tscp = SysTime.rdtscp();
             long cpuid_tsc = SysTime.cpuidrdtsc();
-//            long sysTime = SysTime.clocktime();
 
             System.out.println();
             System.out.printf("System.millis          =%,d\n",msNow);
             System.out.printf("System.nanoTime        =%,d\n",nsNow);
-//            System.out.printf("SysTime.clocktime      =%,d\n",sysTime);
             System.out.printf("SysTime.cpuid_rdtsc    =%,d\n",cpuid_tsc);
             System.out.printf("SysTime.rdtsc          =%,d\n",tsc);
             System.out.printf("SysTime.rdtscp         =%,d\n",tscp);
